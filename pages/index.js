1a134d6bde513646b8a7789520b50088c36faf24
Janet Mendez <janetmendez.03@gmail.com>
2017-04-24 00:39:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
+import Navbar from '../components/Navbar'
 
 const Index = () => (
-  <div>
-    <h1>Hello World!</h1>
-  </div>
+  <DefaultLayout>
+  	<Navbar />
+    <h1>Its working</h1>
+    <p>Hurrah!</p>
+    <h2>DO this!</h2>
+    <style jsx>{`
+      div{
+      	background-color: #121212;
+      	height: 20
+      }
+    `}</style>
+  </DefaultLayout>
 )
 
 export default Index
